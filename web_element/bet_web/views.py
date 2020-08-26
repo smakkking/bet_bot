6dcd3729653348013209cb175f8d9ee476a9ead0
Andrey <smak_ing@mail.ru>
2020-08-26 13:59:55 +0300

    что-то работает

--- web_element/bet_web/views.py
+++ web_element/bet_web/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django import views
 from django.middleware.csrf import get_token
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 
 class BotSettings(LoginRequiredMixin, views.View) :
     def get(self, request) :
@@ -17,3 +16,7 @@ class BotSettings(LoginRequiredMixin, views.View) :
 class BotMenu(LoginRequiredMixin, views.View) :
     def get(self, request) :
         return HttpResponse('Hello! i am main menu')
+
+        
+
+        
