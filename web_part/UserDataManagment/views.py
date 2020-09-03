ae5bc471be3d73b5dc059efd9c0aa7e26f159257
Andrey <smak_ing@mail.ru>
2020-09-03 23:19:30 +0300

    реализация первого открытия браузера v1

--- web_part/UserDataManagment/views.py
+++ web_part/UserDataManagment/views.py
@@ -23,6 +23,7 @@ class BotSettings(LoginRequiredMixin, views.View) :
             basic_form.save()
         return render(request, 'bot_set.html', {'form' : basic_form})
 
+
 class BotMenu(LoginRequiredMixin, views.View) :
     def get(self, request) :
         # задать константный путь к файлу json
@@ -31,17 +32,21 @@ class BotMenu(LoginRequiredMixin, views.View) :
         })
         return render(request, 'menu.html', {'form' : basic_form})
 
+
 class UpdateData(LoginRequiredMixin, views.View) :
     def get(self, request) :
         json_path = sys.path[0].replace('web_part', r'user_data\group_post_data.json')
-        file = open(json_path, 'r')
-        data = json.load(file)
-        new_data = {}
-        user_data = SettingsForm(instance=request.user).__dict__['initial']
-        for key in data.keys() :
-            if key in user_data.keys() and user_data[key] :
-                new_data[key] = data[key]
+        with open(json_path, 'r') as f :
+            data = json.load(f)
+            new_data = {}
+            new_data['open_browser'] = dict(request.GET)['new_iteration'][0] == 'true'
+            user_data = SettingsForm(instance=request.user).__dict__['initial']
+            for key in data.keys() :
+                if key in user_data.keys() and user_data[key] :
+                    new_data[key] = data[key]
+            
         return JsonResponse(new_data, safe=False)
+
 
 class BetData(LoginRequiredMixin, views.View) :
     def get(self, request) :
@@ -50,7 +55,7 @@ class BetData(LoginRequiredMixin, views.View) :
             for key in data.keys() :
                 if key in expected_keys and data[key][0] != 'old':
                     data[key] = json.loads(data[key][0])
-
+        
         bet_info = dict(request.GET)
         del bet_info['_']
         parse_json(bet_info)
