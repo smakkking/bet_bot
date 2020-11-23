6d31fddc489babdfa87d121330324ac0c397e01f
Andrey <smak_ing@mail.ru>
2020-11-23 13:26:18 +0300

    parallel works

--- web_part/EXE_scripts/load_last_data.py
+++ web_part/EXE_scripts/load_last_data.py
@@ -6,9 +6,7 @@ from moduls.bet_manage import YandexAPI_detection
 import json, nltk, time, functools
 from multiprocessing import Pool
 
-OLD_DATA = {}
-
-def load_last_data(group_off, OLD_DATA) :
+def load_last_data(OLD_DATA, token, group_off) :
     # где создавать токен?
     post = bet_manage.LastGroupPost(GROUP_OFFSET[group_off].WALL_URL)
     try :
@@ -16,15 +14,15 @@ def load_last_data(group_off, OLD_DATA) :
         if (group_off in OLD_DATA.keys()) and OLD_DATA[group_off]['text'] == post.text :
             post.parse_bet = False
         else :
-            check_templates(GROUP_OFFSET[group_off], post)
-    except Exception as e:
+            check_templates(GROUP_OFFSET[group_off], post, token)
+    except:
         post.parse_bet = False
     return (group_off, post.__json_repr__())
 
-def check_templates(group_module, post) :
+def check_templates(group_module, post, token) :
     for photo_url in post.photo_list :
-        obj = YandexAPI_detection(photo_url)
-        text = obj.text_detection()
+        obj = YandexAPI_detection(photo_url, token)
+        text = obj.text_detection(debug=True)
         for (tmp, parse) in group_module.BET_TEMPLATES :
             if (tmp(text.upper())) :
                 st = parse(photo_url, nltk.word_tokenize(text))
@@ -39,15 +37,9 @@ def main() :
 
     with open(ALL_POSTS_JSON_PATH, 'r') as last_posts_json :
         OLD_DATA = json.load(last_posts_json)
-    # паралельно работать не хочет
 
-    new_data = []
-    for g in GROUP_OFFSET.keys() : 
-        new_data.append(load_last_data(g, OLD_DATA))
-    new_data = dict(new_data)
-
-    #with Pool(processes=len(GROUP_OFFSET.values())) as pool :
-    #    new_data = dict(pool.map(load_last_data, GROUP_OFFSET.keys()))
+    with Pool(processes=len(GROUP_OFFSET.values())) as pool :
+        new_data = dict(pool.map(functools.partial(load_last_data, OLD_DATA, YandexAPI_detection.iam_token), GROUP_OFFSET.keys()))
 
     with open(ALL_POSTS_JSON_PATH, 'w') as last_posts_json :
         json.dump(new_data, last_posts_json, indent=4)
